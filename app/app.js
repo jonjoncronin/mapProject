db994231c765d0cb5a6a782a93acb1bb037ebf8a
Jon Cronin <jonjoncronin@gmail.com>
2017-10-19 20:54:35 -0700

    Create the base file organization for app.js

---
+++ app/app.js
@@ -0,0 +1,26 @@
+/**
+* @file app.js
+* @description The app.js file defines the Model and ViewModel for a Model -
+* View - ViewModel based Single Page Application that features a map of a
+* neighborhood. This SPA includes highlighted locations, and utilizes
+* third-party data about those locations and various ways to browse the content.
+* @author Jon Cronin
+**/
+
+"use strict";
+
+/**
+* Model Definitions
+**/
+
+/**
+* ViewModel definitions
+**/
+var ViewModel = function () {
+  var self = this;
+};
+
+/*
+* Activate Knockout for the Application
+**/
+ko.applyBindings(new ViewModel());
