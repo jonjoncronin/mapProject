fd5359f7049b9a7d6187f34f6cac508ae135a5bc
Jon Cronin <jonjoncronin@gmail.com>
2017-11-12 14:35:38 -0800

    Get locations for keywords from google Places API and store them to be used.

--- app/app.js
+++ app/app.js
@@ -19,60 +19,36 @@ var filters = ['All Locations',
                'Mexican Restaurants',
                'Parks'];
 
-var locations = [];
+var golfLocations = [];
+var donutLocations = [];
+var brewLocations = [];
+var mexicanLocations = [];
+var parkLocations = [];
+var allLocations = []
+var locations;
 
 function getLocations(someFilter) {
   switch(someFilter){
     case "Golf Courses":
-    locations = ['Golf Courses',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations'];
+      return golfLocations;
     break;
     case "Donuts":
-    locations = ['Donuts',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations'];
+      return donutLocations;
     break;
     case "Breweries":
-    locations = ['Breweries',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations'];
+      return brewLocations;
     break;
     case "Mexican Restaurants":
-    locations = ['Mexican Restaurants',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations'];
+      return mexicanLocations;
     break;
     case "Parks":
-    locations = ['Parks',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations'];
+      return parkLocations;
     break;
     default:
-    locations = ['All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations',
-                 'All Locations'];
+      return allLocations;
     break;
   }
-  return locations;
+  return [];
 };
 
 /**
@@ -106,7 +82,66 @@ ko.applyBindings(new ViewModel());
 * Google Maps work
 **/
 var map;
+// expected latLong for Rocklin is lat: 38.7907339, long: -121.23578279999998
 var defaultMapCenter = "Rocklin, CA";
+
+function populateLocations(map) {
+  filters.forEach(function(filter) {
+    if(filter == "All Locations") {
+      return;
+    }
+    var request = {
+      location: {lat: 38.7907339, lng: -121.23578279999998},
+      radius: '8000',
+      keyword: filter
+    };
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, function(results,status) {
+      if(status == 'OK') {
+        console.log("Places API call worked");
+        for(var ii = 0;ii<results.length;ii++)
+        {
+          if(ii>4) {
+            // only handle at most the first 5 entries
+            break;
+          }
+          console.log(results[ii]);
+          // take the result and store it in the applicable location array
+          var locObj = {name: results[ii].name,
+                        url: 'http://example.com'};
+          switch(filter) {
+            case "Golf Courses":
+              golfLocations.push(locObj);
+            break;
+            case "Donuts":
+              donutLocations.push(locObj);
+            break;
+            case "Breweries":
+              brewLocations.push(locObj);
+            break;
+            case "Mexican Restaurants":
+              mexicanLocations.push(locObj);
+            break;
+            case "Parks":
+              parkLocations.push(locObj);
+            break;
+            default:
+            break;
+          }
+        }
+        // now populate all locations array
+        allLocations = golfLocations.concat(donutLocations,
+                                            brewLocations,
+                                            mexicanLocations,
+                                            parkLocations);
+      }
+      else {
+        console.log("Places API call didn't like you");
+        console.log(status);
+      }
+    });
+  });
+};
 
 function geocodeBaseCity(address, map) {
   var geocoder = new google.maps.Geocoder();
@@ -126,5 +161,5 @@ function initMap() {
   };
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
   geocodeBaseCity(defaultMapCenter, map);
-  populateLocations(filters);
+  populateLocations(map);
 };
