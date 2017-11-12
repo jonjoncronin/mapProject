589ed1613ca378197dd0856f800518c6bcc91827
Jon Cronin <jonjoncronin@gmail.com>
2017-11-11 18:42:12 -0800

    Build event skeleton for selecting filters

--- app/app.js
+++ app/app.js
@@ -19,6 +19,61 @@ var filters = ['All Locations',
                'Mexican Restaurants',
                'Parks'];
 
+var locations = [];
+
+function getLocations(someFilter) {
+  switch(someFilter){
+    case "Golf Courses":
+    locations = ['Golf Courses',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations'];
+    break;
+    case "Donuts":
+    locations = ['Donuts',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations'];
+    break;
+    case "Breweries":
+    locations = ['Breweries',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations'];
+    break;
+    case "Mexican Restaurants":
+    locations = ['Mexican Restaurants',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations'];
+    break;
+    case "Parks":
+    locations = ['Parks',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations'];
+    break;
+    default:
+    locations = ['All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations',
+                 'All Locations'];
+    break;
+  }
+  return locations;
+};
 
 /**
 * ViewModel definitions
@@ -26,17 +81,35 @@ var filters = ['All Locations',
 var ViewModel = function () {
   var self = this;
 
-  this.filterList = ko.observableArray([]);
+  self.filterList = ko.observableArray([]);
 
   filters.forEach(function(filter) {
     self.filterList.push(filter);
   });
 
-  this.currentFilter = ko.observable( this.filterList()[0] );
-  this.locationList = ko.observableArray([]);
+  self.selectedFilter = ko.observable( self.filterList()[0] );
+
+  self.locationList = ko.computed(function () {
+    console.log("Changing locations list for - ");
+    console.log(this.selectedFilter());
+    return getLocations(this.selectedFilter());
+  }, self);
+
 };
 
 /*
 * Activate Knockout for the Application
 **/
 ko.applyBindings(new ViewModel());
+
+/*
+* Google Maps work
+**/
+var map;
+function initMap() {
+  // create a map centered on Rocklin, CA
+  map = new google.maps.Map(document.getElementById('map'), {
+     center: {lat: 38.8118916, lng: -121.2801757},
+     zoom: 12
+  });
+};
